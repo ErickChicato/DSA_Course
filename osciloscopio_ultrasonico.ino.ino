e5490a2dae685f55675ac6cbdc2946552f5236d5
erickchicatto1 <e57072271@gmail.com>
2024-07-16 16:24:43 -0600

    Experiment with a ultrasonic sensor modified

--- osciloscopio_ultrasonico.ino.ino
+++ osciloscopio_ultrasonico.ino.ino
@@ -1,6 +1,16 @@
 #define TRIGGER_PIN 2
 #define ECHO_PIN 4
 
+int stack[15];
+int size;
+
+
+long duration, distance;
+
+void push();
+void pop();
+void display();
+
 void setup() {
   Serial.begin(9600);
   pinMode(TRIGGER_PIN, OUTPUT);
@@ -8,7 +18,7 @@ void setup() {
 }
 
 void loop() {
-  long duration, distance;
+  
   
   digitalWrite(TRIGGER_PIN, LOW);
   delayMicroseconds(2);
@@ -20,9 +30,54 @@ void loop() {
   
   distance = (duration * 0.0343) / 2; // La velocidad del sonido es de aproximadamente 0.0343 cm/µs
   
+
+  if (distance >= 5){
+    push();
+  }
+  display();
+
+  if(distance <= 5){
+    pop();
+  }
+  display();
+
+
   Serial.print("Distancia: ");
   Serial.print(distance);
   Serial.println(" cm");
   
   delay(1000);
 }
+
+void push(){
+  if(distance==size-1){
+    Serial.println("\n Stack is overflow");
+  }
+  else{
+    stack[distance]; // To store the distance 
+  }
+
+}
+
+void display(){
+  int i;
+
+  for(i=0;i<=distance;i++){
+    Serial.println(" distance ");
+    Serial.println(distance[i]);
+  }
+
+
+}
+
+void pop(){
+
+  int temp;
+  temp = stack[distance];
+  distance--;
+
+
+}
+
+
+
