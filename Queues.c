4a8ef9669033ae6a0f3a686cfda4bf28c4b9ea76
erickchicatto1 <e57072271@gmail.com>
2024-08-06 19:10:58 -0600

    adding more functinalities to the queue

--- Queues.c
+++ Queues.c
@@ -79,4 +79,85 @@ void Enqueue(int val){
         queue[rear]=val;
     }
 }
+//https://www.scaler.com/topics/queue-in-c/
 
+// Function to dequeue or pop an element from the queue
+void Dequeue(void){
+
+  // If the queue was empty we cannot pop any element out of it
+  // We return that the underflow condition has been encountered
+  if(isEmpty()==true){
+      printf("\nUnderflow Condition Encountered\n");
+  }
+
+  else{
+    // Print the popped element
+    printf("Dequeued/Popped : %d\n",queue[front]);
+    front++;
+    
+    // Resetting the queue when the last item is popped from the queue
+    if(front>rear){
+      front = rear = -1;
+    }
+  }
+
+
+}
+
+//Function to print the queue
+void display(void){
+
+  //If the queue is already empty 
+  if(rear == -1){
+    printf("Queue is empty\n");
+  }
+
+  else{
+    // A variable to help in iteration through the queue.
+    int i;
+    printf("\nPrinting the Queue :\n");
+
+    // Printing all the elements of the queue
+    for(i=front ; i<=rear;i++){
+
+      printf("%d",queue[i]);
+
+    }
+    printf("\n");
+  }
+
+}
+
+
+int main()
+{
+    // Checking if the queue is empty
+    if (isEmpty())
+        printf("The queue is empty\n");
+
+    // Assigning elements to the queue
+    Enqueue(1);
+    Enqueue(28);
+    Enqueue(5);
+    Enqueue(64);
+
+    // Printing the first element of the queue
+    peek();
+
+    // Printing the size of the queue
+    printf("The size of the queue is %d\n", size());
+
+    // Printing the queue
+    display();
+
+    // Popping the elements from the queue
+    Dequeue();
+    Dequeue();
+
+    // Printing the size of the queue
+    printf("The size of the queue is %d\n", size());
+
+    // Printing the queue
+    display();
+    return 0;
+}
